213c85cf406c6e582e006d8d97ff7bc6fef770cb
Zhongshan <40972895+zhongshanxu@users.noreply.github.com>
2023-10-15 23:04:31 +0800

    Add files via upload

---
+++ ProjectSoliton_Backreaction/v2.3/O2_Comments/jacobir0.m
@@ -0,0 +1,6 @@
+{{eye, -eye, zero, zero, zero, zero, zero}, {zero, Subscript[d, 1, 0], zero, 
+  zero, zero, zero, zero}, {zero, zero, Subscript[d, 1, 0], zero, zero, zero, 
+  zero}, {zero, zero, zero, Subscript[d, 1, 0], zero, zero, zero}, 
+ {zero, zero, zero, zero, Subscript[d, 1, 0], zero, zero}, 
+ {zero, zero, zero, zero, zero, Subscript[d, 1, 0], zero}, 
+ {zero, zero, zero, zero, zero, zero, Subscript[d, 1, 0]}}
